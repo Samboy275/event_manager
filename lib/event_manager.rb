6bf02ead1a1109ffdb7334748bb16b5ee89a2389
samboy275 <sam-wal@windowslive.com>
2023-07-17 17:10:30 +0300

    Add google api client to get ligislators info add a function to parse legislator names for each attendee

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,45 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   # cleans zipcodes and truncates 0's at the start untill it reaches a five digit number
   return zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+
+def display_legislators_by_zipcode(zipcode)
+  # initlizing google civi_info client
+  civi_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civi_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  # gets a string of legislators if zip code was given correctly
+  begin
+    # getting a response object with legislators
+    legislators = civi_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+
+    # parsing names from response object
+    legislators = legislators.officials
+
+    legislators_names = legislators.map(&:name)
+
+    legislator_string = legislators_names.join(', ')
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  legislator_string
+
+end
+
+
+puts "event manager initilized"
+
+
+
 
 attendee_list = CSV.open(
   '../event_attendees.csv',
@@ -15,5 +51,6 @@ attendee_list = CSV.open(
 attendee_list.each do |line|
   first_name = line[:first_name]
   zipcode = clean_zipcode(line[:zipcode])
-  puts "#{first_name}, #{zipcode}"
+
+  puts "#{first_name}, #{zipcode}, #{display_legislators_by_zipcode(zipcode)}"
 end
